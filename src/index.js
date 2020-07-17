4f0ee010fab119e3516d68fc692ec5442aeaad69
Anton Shafner <ashafner@tsconsulting.com>
2020-07-17 14:55:06 +0300

    отрисовал главную страницу

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App/App.jsx";
 
+const init = () => {
+  const promo = {
+    title: `The Grand Budapest Hotel`,
+    genre: `Drama`,
+    year: 2014,
+  };
+  ReactDOM.render(
+      <App promoTitle={promo.title} promoGenre={promo.genre} promoYear={promo.year}/>,
+      document.querySelector(`#root`)
+  );
+};
+
+init();
