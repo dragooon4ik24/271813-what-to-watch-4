c1b0f82818fe768b48331dbd78d330ac94e346cc
Anton Shafner <ashafner@tsconsulting.com>
2020-07-17 18:41:51 +0300

    добавил проверку через prop types

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Main from "../Main/Main.jsx";
 
-// eslint-disable-next-line react/prop-types
-export default function App({promoTitle, promoGenre, promoYear}) {
-  return <Main title={promoTitle} genre={promoGenre} year={promoYear}/>;
+export default function App({promoTitle, promoGenre, promoYear, films}) {
+  return (
+    <Main
+      title={promoTitle}
+      genre={promoGenre}
+      year={promoYear}
+      films={films}
+    />
+  );
 }
+
+App.propTypes = {
+  promoTitle: PropTypes.string.isRequired,
+  promoGenre: PropTypes.string.isRequired,
+  promoYear: PropTypes.number.isRequired,
+  films: PropTypes.arrayOf(PropTypes.string),
+};
